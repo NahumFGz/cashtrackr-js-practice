fdfc72f0160753faae8eaf1f89d19abcd5a08a09
NahumFGz <ngutierrez2507@gmail.com>
2025-03-28 04:05:30 -0500

    feat: :sparkles: Creando el primer modelo

--- b_tutorial/backend/src/config/db.ts
+++ b_tutorial/backend/src/config/db.ts
@@ -1,6 +1,11 @@
 import { Sequelize } from "sequelize-typescript"
 
 export const db = new Sequelize( process.env.DB_URL, {
+    models: [__dirname + '/../models/**/*'],
+    logging: false,
+    // define: {
+    //     timestamps: false
+    // }
     //! Para conexiones de servicios externos
     // dialectOptions: {
     //     ssl: {
