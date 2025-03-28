80367e884ced2f5d5e4826186db10ebc6714bb6f
NahumFGz <ngutierrez2507@gmail.com>
2025-03-28 04:25:01 -0500

    feat: :sparkles: Creando la estructura de controllers y routes

--- b_tutorial/backend/src/server.ts
+++ b_tutorial/backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import colors from 'colors'
 import morgan from 'morgan'
 import { db } from './config/db'
+import budgetRouter from './routes/budgetRouter'
 
 async function connectDB() {
   try {
@@ -9,7 +10,6 @@ async function connectDB() {
     db.sync()
     console.log(colors.blue.bold('Conexión exitosa a la BD'))
   } catch (error) {
-    // console.log(error)
     console.log(colors.red.bold('Falló la conexión a la BD'))
   }
 }
@@ -20,5 +20,7 @@ const app = express()
 app.use(morgan('dev'))
 
 app.use(express.json())
+
+app.use('/api/budgets', budgetRouter)
 
 export default app
