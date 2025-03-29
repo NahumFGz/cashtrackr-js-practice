bf79738120665609348f5d95358bca3362ae6d74
NahumFGz <ngutierrez2507@gmail.com>
2025-03-29 18:50:02 -0500

    feat: :zap: Validaciones de existencia de usuario

--- b_tutorial/backend/src/controllers/AuthController.ts
+++ b_tutorial/backend/src/controllers/AuthController.ts
@@ -3,6 +3,14 @@ import User from '../models/User'
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
+    const { email } = req.body
+    // Prevenir duplicados
+    const userExists = await User.findOne({ where: { email: email } })
+    if (userExists) {
+      const error = new Error('El correo ya está registrado')
+      return res.status(409).json({ error: error.message })
+    }
+
     try {
       const user = new User(req.body)
       await user.save()
