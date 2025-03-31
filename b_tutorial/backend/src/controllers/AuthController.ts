60272d79348654d79f4d84a582a1b5d60a0144bf
NahumFGz <ngutierrez2507@gmail.com>
2025-03-31 17:49:24 -0500

    feat: :sparkles: Primeros pasos de validación de JWT

--- b_tutorial/backend/src/controllers/AuthController.ts
+++ b_tutorial/backend/src/controllers/AuthController.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
 import User from '../models/User'
 import { checkPassword, hashPassword } from '../utils/auth'
 import { generateToken } from '../utils/token'
@@ -126,6 +127,31 @@ export class AuthController {
   }
 
   static user = async (req: Request, res: Response) => {
-    res.json(req.headers.authorization)
+    const bearer = req.headers.authorization
+
+    if (!bearer) {
+      const error = new Error('No autorizado')
+      return res.status(401).json({ error: error.message })
+    }
+
+    const [, token] = bearer.split(' ')
+    if (!token) {
+      const error = new Error('Token no valido')
+      return res.status(401).json({ error: error.message })
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+      if (typeof decoded === 'object' && decoded.id) {
+        const user = await User.findByPk(decoded.id, {
+          attributes: ['id', 'name', 'email'],
+        })
+
+        res.json(user)
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'Token no válido' })
+    }
   }
 }
