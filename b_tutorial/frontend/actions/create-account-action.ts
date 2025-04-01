b2bd0e14683a327ec3e361ae5e94012f6e756975
NahumFGz <ngutierrez2507@gmail.com>
2025-04-01 18:48:25 -0500

    feat: :sparkles: Se agrega zod para validar el formulario

--- b_tutorial/frontend/actions/create-account-action.ts
+++ b_tutorial/frontend/actions/create-account-action.ts
@@ -1,5 +1,23 @@
 'use server'
 
-export async function register() {
-  console.log('Desde Server Action')
+import { RegisterSchema } from '@/src/schemas'
+
+export async function register(formData: FormData) {
+  console.log(formData)
+
+  const registerData = {
+    email: formData.get('email'),
+    name: formData.get('name'),
+    password: formData.get('password'),
+    password_confirmation: formData.get('password_confirmation'),
+  }
+  // Validar
+  const register = RegisterSchema.safeParse(registerData)
+  console.log('register', register)
+  console.log('register.error', register.error)
+
+  const error = register.error?.errors.map((error) => error.message)
+  console.log('error', error)
+
+  // Registrar el usuario
 }
--- b_tutorial/frontend/z-next-notes.md
+++ b_tutorial/frontend/z-next-notes.md
@@ -3,3 +3,6 @@
 - Next 14
   npx create-next-app@next-14
   npx create-next-app@latest
+
+- Validador y completado
+  npm i zod
