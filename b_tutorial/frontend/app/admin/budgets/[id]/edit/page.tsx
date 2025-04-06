6c58ac98c005737ee6ce752647a36c7c2244d8b5
NahumFGz <ngutierrez2507@gmail.com>
2025-04-06 02:28:08 -0500

    feat: :sparkles: Se agrega el formulario de presupuesto al editar

--- b_tutorial/frontend/app/admin/budgets/[id]/edit/page.tsx
+++ b_tutorial/frontend/app/admin/budgets/[id]/edit/page.tsx
@@ -1,4 +1,7 @@
+import Link from 'next/link'
 import getToken from '@/src/auth/token'
+import { BudgetAPIResponseSchema } from '@/src/schemas'
+import { notFound } from 'next/navigation'
 
 const getBudget = async (budgetId: string) => {
   const token = getToken()
@@ -11,9 +14,12 @@ const getBudget = async (budgetId: string) => {
 
   const json = await req.json()
   if (!req.ok) {
+    notFound()
   }
 
-  return json
+  const budget = BudgetAPIResponseSchema.parse(json)
+
+  return budget
 }
 
 export default async function EditBudgetPage({
@@ -22,11 +28,28 @@ export default async function EditBudgetPage({
   params: { id: string }
 }) {
   const { id } = params
+  const budget = await getBudget(id)
 
-  getBudget(id)
   return (
     <>
-      <p>EditBudgetPage</p>
+      <div className='flex flex-col-reverse md:flex-row md:justify-between items-center'>
+        <div className='w-full md:w-auto'>
+          <h1 className='font-black text-4xl text-purple-950 my-5'>
+            Editar Presupuesto: {budget.name}
+          </h1>
+          <p className='text-xl font-bold'>
+            Llena el formulario y crea un nuevo {''}
+            <span className='text-amber-500'>presupuesto</span>
+          </p>
+        </div>
+        <Link
+          href={'/admin'}
+          className='bg-amber-500 p-2 rounded-lg text-white font-bold w-full md:w-auto text-center'
+        >
+          Volver
+        </Link>
+      </div>
+      <div className='p-10 mt-10  shadow-lg border '></div>
     </>
   )
 }
