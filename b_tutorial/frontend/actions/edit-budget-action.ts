6a60bfbbd8655c30eef547578c132061fcf3358c
NahumFGz <ngutierrez2507@gmail.com>
2025-04-06 15:39:47 -0500

    feat: :sparkles: Se agrega el PUT

--- b_tutorial/frontend/actions/edit-budget-action.ts
+++ b_tutorial/frontend/actions/edit-budget-action.ts
@@ -1,6 +1,12 @@
 'use server'
 
-import { Budget } from '@/src/schemas'
+import getToken from '@/src/auth/token'
+import {
+  Budget,
+  DraftBudgetSchema,
+  ErrorResposeSchema,
+  SuccessSchema,
+} from '@/src/schemas'
 
 type ActionStateType = {
   errors: string[]
@@ -12,8 +18,46 @@ export async function editBudget(
   prevState: ActionStateType,
   formData: FormData
 ) {
+  const budgetData = {
+    name: formData.get('name'),
+    amount: formData.get('amount'),
+  }
+  const budget = DraftBudgetSchema.safeParse(budgetData)
+  if (!budget.success) {
+    return {
+      errors: budget.error.issues.map((issue) => issue.message),
+      success: '',
+    }
+  }
+
+  //!Enviar petición
+  const token = getToken()
+  const url = `${process.env.API_URL}/budgets/${budgetId}`
+  const req = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      name: budget.data.name,
+      amount: budget.data.amount,
+    }),
+  })
+  const json = await req.json()
+
+  if (!req.ok) {
+    const { error } = ErrorResposeSchema.parse(json)
+    return {
+      errors: [error],
+      success: '',
+    }
+  }
+
+  const success = SuccessSchema.parse(json)
+
   return {
     errors: [],
-    success: '',
+    success: success,
   }
 }
