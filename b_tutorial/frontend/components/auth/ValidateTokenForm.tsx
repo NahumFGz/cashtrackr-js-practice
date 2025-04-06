705dc60b2d1f2c9693825410e155ce8b150b0107
NahumFGz <ngutierrez2507@gmail.com>
2025-04-05 22:49:40 -0500

    feat: :sparkles: Mostrando los errores de tokens invalidos

--- b_tutorial/frontend/components/auth/ValidateTokenForm.tsx
+++ b_tutorial/frontend/components/auth/ValidateTokenForm.tsx
@@ -2,6 +2,7 @@ import { validateToken } from '@/actions/validate-token-action'
 import { PinInput, PinInputField } from '@chakra-ui/pin-input'
 import { useEffect, useState } from 'react'
 import { useFormState } from 'react-dom'
+import { toast } from 'react-toastify'
 
 export default function ValidateTokenForm() {
   const [token, setToken] = useState('')
@@ -12,6 +13,12 @@ export default function ValidateTokenForm() {
     errors: [],
     success: '',
   })
+
+  useEffect(() => {
+    if (state.errors) {
+      state.errors.forEach((error) => toast.error(error))
+    }
+  }, [state])
 
   useEffect(() => {
     if (isComplete) {
@@ -30,7 +37,11 @@ export default function ValidateTokenForm() {
 
   return (
     <div className='flex justify-center gap-5 my-10'>
-      <PinInput value={''} onChange={handleChange} onComplete={handleComplete}>
+      <PinInput
+        value={token}
+        onChange={handleChange}
+        onComplete={handleComplete}
+      >
         <PinInputField className='h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white' />
         <PinInputField className='h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white' />
         <PinInputField className='h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white' />
