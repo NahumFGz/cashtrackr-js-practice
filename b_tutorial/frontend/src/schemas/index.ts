bfb789d55b9943ca6783b08c98c3b351dc9b314d
NahumFGz <ngutierrez2507@gmail.com>
2025-04-08 18:25:13 -0500

    feat: :sparkles: Corrigiendo el error de zod con .omit

--- b_tutorial/frontend/src/schemas/index.ts
+++ b_tutorial/frontend/src/schemas/index.ts
@@ -102,7 +102,9 @@ export const BudgetAPIResponseSchema = z.object({
   expenses: z.array(ExpenseAPIResponseSchema),
 })
 
-export const BudgetsAPIResponseSchema = z.array(BudgetAPIResponseSchema)
+export const BudgetsAPIResponseSchema = z.array(
+  BudgetAPIResponseSchema.omit({ expenses: true })
+)
 
 export type User = z.infer<typeof UserSchema>
 export type Budget = z.infer<typeof BudgetAPIResponseSchema>
